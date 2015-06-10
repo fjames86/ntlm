ba899ab2cc3974c560ea1a7b73334840a915203d
Frank James <frank.a.james@gmail.com>
2015-06-10 15:29:13 +0100

    don't export loads of symbols noone needs

--- package.lisp
+++ package.lisp
@@ -1,3 +1,5 @@
+;;;; Copyright (c) Frank James 2015 <frank.a.james@gmail.com>
+;;;; This code is licensed under the MIT license.
 
 ;;;; NTLM Authentication library
 
@@ -11,57 +13,57 @@
 
 
 
-(defpackage :ntlm 
-  (:use :cl :packet)
+(defpackage #:ntlm 
+  (:use #:cl #:packet)
   (:export	   
-	   #:pack-negotiate-message
-	   #:unpack-negotiate-message
-	   #:pack-challenge-message
-	   #:unpack-challenge-message
-	   #:pack-authenticate-message
-	   #:unpack-authenticate-message
+	   ;; #:pack-negotiate-message
+	   ;; #:unpack-negotiate-message
+	   ;; #:pack-challenge-message
+	   ;; #:unpack-challenge-message
+	   ;; #:pack-authenticate-message
+	   ;; #:unpack-authenticate-message
 
-	   #:make-ntlm-version
-	   #:make-target-info
-       #:make-single-host
+	   ;; #:make-ntlm-version
+	   ;; #:make-target-info
+       ;; #:make-single-host
 
-	   #:des
-	   #:desl
-	   #:md4
-	   #:md5
-	   #:crc32
-       #:rc4-init
-	   #:rc4
-       #:rc4k
-	   #:hmac-md5
-	   #:lmowf-v1
-	   #:ntowf-v1
-	   #:ntowf-v2
-	   #:lmowf-v2
-	   #:session-base-key-v1
-	   #:session-base-key-v2
-	   #:key-exchange-key
-       #:make-temp
-	   #:lm-response-v1
-	   #:nt-response-v1
-	   #:lm-response-v1*
-	   #:nt-response-v1*
-	   #:lm-response-v2
-	   #:nt-response-v2
-       #:nt-response-v2-list
-	   #:sign-key
-	   #:seal-key
-	   #:mac
-	   #:encrypted-session-key
-	   #:password-md4
-       #:mic
-       #:client-challenge
-       #:server-challenge
-       #:nonce
-       #:exported-session-key
-       #:unix
-       #:filetime
-       #:filetime-unix
+	   ;; #:des
+	   ;; #:desl
+	   ;; #:md4
+	   ;; #:md5
+	   ;; #:crc32
+       ;; #:rc4-init
+	   ;; #:rc4
+       ;; #:rc4k
+	   ;; #:hmac-md5
+	   ;; #:lmowf-v1
+	   ;; #:ntowf-v1
+	   ;; #:ntowf-v2
+	   ;; #:lmowf-v2
+	   ;; #:session-base-key-v1
+	   ;; #:session-base-key-v2
+	   ;; #:key-exchange-key
+       ;; #:make-temp
+	   ;; #:lm-response-v1
+	   ;; #:nt-response-v1
+	   ;; #:lm-response-v1*
+	   ;; #:nt-response-v1*
+	   ;; #:lm-response-v2
+	   ;; #:nt-response-v2
+       ;; #:nt-response-v2-list
+	   ;; #:sign-key
+	   ;; #:seal-key
+	   ;; #:mac
+	   ;; #:encrypted-session-key
+	   ;; #:password-md4
+       ;; #:mic
+       ;; #:client-challenge
+       ;; #:server-challenge
+       ;; #:nonce
+       ;; #:exported-session-key
+       ;; #:unix
+       ;; #:filetime
+       ;; #:filetime-unix
 
        ;; new GSS interface
        #:logon-user
